251918f3135b1a5067b6a7a966b43f51dffa3dcb
Kura <kura.hax@gmail.com>
2026-09-15 17:12:26 +0100

    Grab tag from release

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/sh
-docker buildx build --progress=plain -t git.kura.gg/kura/glitch-mastodon:latest -t git.kura.gg/kura/glitch-mastodon:4.7.2 --platform linux/amd64,linux/arm64 --push -f Containerfile .
+ver=$(curl -sf "https://api.github.com/repos/glitch-soc/mastodon/releases/latest"  | awk '/tag_name/{print $4;exit}' FS='[""]' | sed 's/v//g')
+docker buildx build --progress=plain -t git.kura.gg/kura/glitch-mastodon:latest -t "git.kura.gg/kura/glitch-mastodon:$ver" --platform linux/amd64,linux/arm64 --push -f Containerfile .
